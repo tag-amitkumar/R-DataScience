78318f03db9339b701d0cba74c9ba2f7f5fe6d01
Amit Kumar <namitkumarn@gmail.com>
2020-10-10 09:20:45 +0530

    Update TimeSeries_Analysis.R

--- TimeSeries_Analysis.R
+++ TimeSeries_Analysis.R
@@ -4,17 +4,17 @@ library(dplyr)
 
 install.packages("dataiku")
 
-setwd("C:/HK")
-df = read.csv("Book1.csv")
+setwd("C:/workdir") # set working directory
+df = read.csv("Book1.csv")  # read any data as in dataframe
 
-head(df)
+head(df) # check data formatimg
 
-ts_passengers = ts(df$SeaLevel,
+# pass the start, end and the frequency to perform the time series analysis
+ts_passengers = ts(df$passengers,
                    start=1954,end = 2017, frequency=1)
 plot(ts_passengers)
-
 
 m_ets = ets(ts_passengers)
 f_ets = forecast(m_ets, h=30) # forecast 24 months into the future
 write.csv(f_ets, "book_2.csv")
-plot(f_ets)
+plot(f_ets) # plot the result
